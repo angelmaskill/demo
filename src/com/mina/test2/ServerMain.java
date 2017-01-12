b5d4ef6804869fdc10f5ce5f3ff3f2a32812fb05
angelmaskill <mayanlu@tiamaes-myl>
2017-01-12 13:43:17 +0800

    添加自定义业务线程池.

--- src/com/mina/test2/ServerMain.java
+++ src/com/mina/test2/ServerMain.java
@@ -1,55 +1,70 @@
 package com.mina.test2;
 
-import java.io.IOException;  
-import java.net.InetSocketAddress;  
-import java.net.SocketAddress;  
-import org.apache.mina.core.filterchain.DefaultIoFilterChainBuilder;  
-import org.apache.mina.core.service.IoAcceptor;  
-import org.apache.mina.core.session.IdleStatus;  
-import org.apache.mina.filter.codec.ProtocolCodecFilter;  
-import org.apache.mina.filter.logging.LoggingFilter;  
-import org.apache.mina.transport.socket.nio.NioSocketAcceptor;  
-  
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.SocketAddress;
+import java.util.concurrent.Executors;
+
+import org.apache.mina.core.filterchain.DefaultIoFilterChainBuilder;
+import org.apache.mina.core.service.IoAcceptor;
+import org.apache.mina.core.session.IdleStatus;
+import org.apache.mina.filter.codec.ProtocolCodecFilter;
+import org.apache.mina.filter.executor.ExecutorFilter;
+import org.apache.mina.filter.logging.LoggingFilter;
+import org.apache.mina.transport.socket.nio.NioSocketAcceptor;
+
 /**
- *  I/O Service 扮演实际的IO角色,接收,发送网络IO
-	I/O Filter Chain 在输入和输出的io流中进行过滤
-	I/O Handler 业务逻辑实现
+ * <pre>
+ *  IoAcceptor用于监听客户端的连接
+ *  IoProcessor线程用于IO的处理
+ * 	I/O Filter Chain 在输入和输出的io流中进行过滤
+ * 	I/O Handler 业务逻辑实现
+ * 	
+ * 	各种线程的产生 :
+ * 	当 IoAcceptor/IoConnector实例创建的时候，同时一个关联在IoAcceptor/IoConnector上的IoProcessor线程池也被创建。
+ * 	当IoAcceptor/IoConnector建立套接字（IoAcceptor 的bind()或者是IoConnector 的connect()方法被调用）时，从线程池中取出一个线程，监听套接字端口。
+ * 	当 IoAcceptor/IoConnector监听到套接字上有连接请求时，建立IoSession 对象，从IoProcessor池中取出一个IoProcessor线程执行IO处理。
+ * 	如若过滤器中配置了“threadPool”过滤器，则使用此线程池建立线程执行业务逻辑（IoHandler）处理，否则使用IoProcessor线程处理业务逻辑。
+ * </pre>
+ * 
  * @author mayanlu
  *
  */
-public class ServerMain {  
-      
-    public static void main(String[] args) throws IOException {  
-          
-        //  在服务器端创建一个监听连接的接收器 基于tcp/ip  
-        IoAcceptor acceptor = new NioSocketAcceptor();  
-          
-        //  绑定的端口  
-        SocketAddress address = new InetSocketAddress("localhost", 8888);  
-          
-        //   获取过滤器链  
-        DefaultIoFilterChainBuilder chain = acceptor.getFilterChain();  
-          
-        //  添加日志过滤器  
-        chain.addLast("logger", new LoggingFilter());  
-          
-        //　配置自定义的编解码器　  
-        chain.addLast("mycodec", new ProtocolCodecFilter(new MyCoderFactory()));  
-          
-        //  添加数据处理的处理器  
-        acceptor.setHandler(new ServerHandler());  
-          
-        //  进行配置信息的设置       
-        acceptor.getSessionConfig().setReadBufferSize(100);       
-        acceptor.getSessionConfig().setIdleTime(IdleStatus.BOTH_IDLE, 10);   
-          
-        /**
-         *   绑定服务器端口,每次监听一个端口,都启动一个线程.
-         *   针对一个端口的请求,只有一个线程来处理IO
-         */
-        acceptor.bind(address);  
-          
-        System.out.println("服务器开始在 8888 端口监听.......");  
-        
-    }  
-}  
+public class ServerMain {
+
+	public static void main(String[] args) throws IOException {
+
+		// 在服务器端创建一个监听连接的接收器 基于tcp/ip
+		IoAcceptor acceptor = new NioSocketAcceptor();
+
+		// 绑定的端口
+		SocketAddress address = new InetSocketAddress("localhost", 8888);
+
+		// 获取过滤器链
+		DefaultIoFilterChainBuilder chain = acceptor.getFilterChain();
+
+		// 添加日志过滤器
+		chain.addLast("logger", new LoggingFilter());
+
+		// 　配置自定义的编解码器　
+		chain.addLast("mycodec", new ProtocolCodecFilter(new MyCoderFactory()));
+
+		// 配置自己的业务线程池
+		chain.addLast("threadPool", new ExecutorFilter(Executors.newCachedThreadPool()));
+
+		// 添加数据处理的处理器
+		acceptor.setHandler(new ServerHandler());
+
+		// 进行配置信息的设置
+		acceptor.getSessionConfig().setReadBufferSize(100);
+		acceptor.getSessionConfig().setIdleTime(IdleStatus.BOTH_IDLE, 10);
+
+		/**
+		 * 绑定服务器端口,每次监听一个端口,都启动一个线程. 针对一个端口的请求,只有一个线程来处理IO
+		 */
+		acceptor.bind(address);
+
+		System.out.println("服务器开始在 8888 端口监听.......");
+
+	}
+}

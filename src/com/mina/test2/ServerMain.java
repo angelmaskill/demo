1ee96e38b93716193f3d67baeb5fb462059c3814
angelmaskill <mayanlu@tiamaes-myl>
2017-02-15 18:05:00 +0800

    添加业务逻辑线程池,可以保证任务的有序性.

--- src/com/mina/test2/ServerMain.java
+++ src/com/mina/test2/ServerMain.java
@@ -10,21 +10,23 @@ import org.apache.mina.core.service.IoAcceptor;
 import org.apache.mina.core.session.IdleStatus;
 import org.apache.mina.filter.codec.ProtocolCodecFilter;
 import org.apache.mina.filter.executor.ExecutorFilter;
+import org.apache.mina.filter.executor.OrderedThreadPoolExecutor;
 import org.apache.mina.filter.logging.LoggingFilter;
 import org.apache.mina.transport.socket.nio.NioSocketAcceptor;
 
 /**
  * <pre>
- *  IoAcceptor用于监听客户端的连接
- *  IoProcessor线程用于IO的处理
- * 	I/O Filter Chain 在输入和输出的io流中进行过滤
- * 	I/O Handler 业务逻辑实现
+ *  IoConnector 		这个地方用于与服务端建立连接，每连接一个服务端（每调用一次connect()方法），就启用一个线程，我们不能改变
+ *  IoAcceptor  		用于监听客户端的连接,每监听一个端口（每调用一次bind()方法），都启用一个线程，这个数字我们不能改变
+ *  IoProcessor 		线程用于IO的处理
+ * 	I/O Filter Chain 	在输入和输出的io流中进行过滤
+ * 	I/O Handler 		业务逻辑实现
  * 	
  * 	各种线程的产生 :
- * 	当 IoAcceptor/IoConnector实例创建的时候，同时一个关联在IoAcceptor/IoConnector上的IoProcessor线程池也被创建。
- * 	当IoAcceptor/IoConnector建立套接字（IoAcceptor 的bind()或者是IoConnector 的connect()方法被调用）时，从线程池中取出一个线程，监听套接字端口。
- * 	当 IoAcceptor/IoConnector监听到套接字上有连接请求时，建立IoSession 对象，从IoProcessor池中取出一个IoProcessor线程执行IO处理。
- * 	如若过滤器中配置了“threadPool”过滤器，则使用此线程池建立线程执行业务逻辑（IoHandler）处理，否则使用IoProcessor线程处理业务逻辑。
+ * 	当 IoAcceptor/IoConnector  实例创建的时候，同时一个关联在IoAcceptor/IoConnector上的IoProcessor线程池也被创建。
+ * 	当IoAcceptor/IoConnector  建立套接字（IoAcceptor 的bind()或者是IoConnector 的connect()方法被调用）时，从线程池中取出一个线程，监听套接字端口。
+ * 	当 IoAcceptor/IoConnector  监听到套接字上有连接请求时，建立IoSession 对象，从IoProcessor池中取出一个IoProcessor线程执行IO处理。
+ * 	如若过滤器中配置了“threadPool”  过滤器，则使用此线程池建立线程执行业务逻辑（IoHandler）处理，否则使用IoProcessor线程处理业务逻辑。
  * </pre>
  * 
  * @author mayanlu
@@ -46,11 +48,13 @@ public class ServerMain {
 		// 添加日志过滤器
 		chain.addLast("logger", new LoggingFilter());
 
-		// 　配置自定义的编解码器　
+		// 配置自定义的编解码器　
 		chain.addLast("mycodec", new ProtocolCodecFilter(new MyCoderFactory()));
 
-		// 配置自己的业务线程池
-		chain.addLast("threadPool", new ExecutorFilter(Executors.newCachedThreadPool()));
+		// 配置自己的业务线程池,不能保证任务处理的顺序性
+		//chain.addLast("threadPool", new ExecutorFilter(Executors.newCachedThreadPool()));
+		//  配置自己的业务线程池,能保证任务处理的顺序性
+		chain.addLast("threadPool", new ExecutorFilter(new OrderedThreadPoolExecutor(500)));
 
 		// 添加数据处理的处理器
 		acceptor.setHandler(new ServerHandler());

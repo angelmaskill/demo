24b34ebb3c9c2cd9f40143f4f54e9982c4761000
AngleMa <870978872@qq.com>
2017-03-16 22:37:12 +0800

    面向对象编程.

---
+++ src/com/BASETEST/oop/demo1/Add.java
@@ -0,0 +1,5 @@
+package com.BASETEST.oop.demo1;
+
+public interface Add {
+	public double add(double a, double b);
+}
---
+++ src/com/BASETEST/oop/demo1/Minus.java
@@ -0,0 +1,5 @@
+package com.BASETEST.oop.demo1;
+
+public interface Minus {
+public double minus(double a,double b);
+}

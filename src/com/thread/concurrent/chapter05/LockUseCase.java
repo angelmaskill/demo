d37fb29599e9c6ed6dc032e42d72e26621621872
angelmaskill <mayanlu@tiamaes-myl>
2017-03-16 16:20:08 +0800

    添加了方腾飞老师的Java并发编程艺术的demo.

---
+++ src/com/thread/concurrent/chapter05/LockUseCase.java
@@ -0,0 +1,18 @@
+﻿package com.thread.concurrent.chapter05;
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * 10-1
+ */
+public class LockUseCase {
+    public void lock() {
+        Lock lock = new ReentrantLock();
+        lock.lock();
+        try {
+        } finally {
+            lock.unlock();
+        }
+    }
+}

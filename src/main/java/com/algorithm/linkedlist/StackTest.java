8f8c758c822d2af05b8405218ff8b880f063dd89
AngelMa <870978872@qq.com>
2021-04-04 21:27:15 +0800

    数组来模拟栈

--- src/main/java/com/algorithm/linkedlist/StackTest.java
+++ src/main/java/com/algorithm/linkedlist/StackTest.java
@@ -10,28 +10,29 @@ public class StackTest {
     public static void main(String[] args) {
         Stack stack = new Stack(5);
         stack.showStack("插入数据之前的栈：");
-        stack.in(0);
-        stack.in(1);
+        stack.push(0);
+        stack.push(1);
         stack.showStack("插入部分数据之后的栈：");
-        stack.in(2);
-        stack.in(3);
-        stack.in(4);
+        stack.push(2);
+        stack.push(3);
+        stack.push(4);
         stack.showStack("插满数据之后的栈：");
         stack.showStack("弹出数据：");
         while (stack.size() > 0) {
-            stack.out();
+            stack.pop();
         }
         stack.showStack("最后栈为：");
     }
 }
 
 class Stack {
-    private int current;
+    // top 表示栈顶，初始值是0
+    private int top = 0;
     private Integer[] stackArray;
     private int size;
 
     public int size() {
-        return current;
+        return top;
     }
 
     public Stack(int size) {
@@ -39,23 +40,31 @@ class Stack {
         stackArray = new Integer[size];
     }
 
-    public void in(Integer value) {
+    /**
+     * 入栈
+     *
+     * @param value
+     */
+    public void push(Integer value) {
         if (!isFull()) {
-            stackArray[current++] = value;
+            stackArray[top++] = value;
         }
     }
 
-    public void out() {
+    /**
+     * 出栈
+     */
+    public void pop() {
         if (!isEmpty()) {
-            System.out.printf("the index %d value %d is pop out!\n", current - 1,
-                stackArray[current - 1].intValue());
-            stackArray[current - 1] = null;
-            current--;
+            System.out.printf("the index %d value %d is pop out!\n", top - 1,
+                stackArray[top - 1].intValue());
+            stackArray[top - 1] = null;
+            top--;
         }
     }
 
     public boolean isFull() {
-        if (current == size) {
+        if (top == size) {
             System.out.println("the stack is full!");
             return true;
         }
@@ -63,7 +72,7 @@ class Stack {
     }
 
     public boolean isEmpty() {
-        if (current == 0) {
+        if (top == 0) {
             System.out.println("the stack is empty!");
             return true;
         }
@@ -73,7 +82,7 @@ class Stack {
     public void showStack(String desc) {
         System.out.println(desc);
         if (!isEmpty()) {
-            for (int i = 0; i < stackArray.length; i++) {
+            for (int i = stackArray.length - 1; i >= 0; i--) {
                 if (stackArray[i] != null) {
                     System.out.printf("the %d index value is %d \n", i, stackArray[i]);
                 }

25b5bde73297ecd8f57f685ba8aa3034b23995c0
mayanlu <mayanlu@newcapec.net>
2021-05-31 17:55:34 +0800

    lambda

---
+++ src/main/java/com/java8/lambda/LambdaPri.java
@@ -0,0 +1,70 @@
+package com.java8.lambda;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Predicate;
+
+/**
+ * @ClassName LambdaPri
+ * @Description lambda 表达式练习
+ * @Author yanlu.myl
+ * @Date 2021-05-31 11:01
+ */
+public class LambdaPri {
+    public static void main(String[] args) {
+        LambdaPri lambdaPri = new LambdaPri();
+        lambdaPri.testFilter();
+
+        // 定义一个入参为空,出参为对象的 lambda 函数
+        LambdaInterface lambdaInterface = () -> 1 == 1;
+        System.out.println(lambdaInterface.apply());
+    }
+
+    private void testThread() {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("before java8");
+            }
+        }).start();
+        //java8 使用lambda表达式
+        new Thread(() -> System.out.println("in java8")).start();
+    }
+
+    private void testCollection() {
+        List<String> features = Arrays.asList("Lambdas", "Default Method",
+                "Stream API", "Date and Time API");
+        //Java 8 之前:
+        for (String feature : features) {
+            System.out.println(feature);
+        }
+        //Java 8使用lambda表达式:
+        features.forEach(str -> System.out.println(str));
+    }
+
+    private void testFilter() {
+        List<String> languages = Arrays.asList("Java", "Scala", "C++", "Haskell", "Lisp");
+        System.out.println("words which start with j:");
+        // 使用lambda 来过滤单词
+        filter2(languages, str -> ((String) str).startsWith("J"));
+        // 使用lambda+stream来过滤单词
+
+    }
+
+    private static void filter(List<String> names, Predicate condition) {
+        for (String name : names) {
+            if (condition.test(name)) {
+                System.out.println(name + " ");
+            }
+        }
+    }
+
+    //java 8 还引入了Stream类
+    private static void filter2(List names, Predicate condition) {
+        names.stream().filter((name) -> (condition.test(name))).forEach((name) -> {
+            System.out.println(name + " ");
+        });
+    }
+
+
+}
--- src/main/java/com/java8/lambda/ThreadLam.java
+++ src/main/java/com/java8/lambda/ThreadLam.java
@@ -1,4 +1,4 @@
-package com.java8;
+package com.java8.lambda;
 
 public class ThreadLam {
     public static void main(String[] args) {

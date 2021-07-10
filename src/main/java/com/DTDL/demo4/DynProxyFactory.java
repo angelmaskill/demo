a73e7b604040b605942a1fd8ae4f140114fad8b5
AngelMa <870978872@qq.com>
2021-07-11 00:17:05 +0800

    动态代理

--- src/main/java/com/DTDL/demo4/DynProxyFactory.java
+++ src/main/java/com/DTDL/demo4/DynProxyFactory.java
@@ -4,17 +4,23 @@ import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Proxy;
 
 /**
- * 生成动态代理对象的工厂.
+ * 类应用关系：代理对象---》handler --》目标对象 生成动态代理对象的工厂.
  */
 public class DynProxyFactory {
-    // 客户类调用此工厂方法获得代理对象。
-    // 对客户类来说，其并不知道返回的是代理类对象还是委托类对象。
+
     public static Subject getInstance() {
-        Subject delegate = new RealSubject();
-        InvocationHandler handler = new SubjectInvocationHandler(delegate);
+
+        /*创建原始对象*/
+        Subject realSubject = new RealSubject();
+
+        /*handler 中需要拿到原始对象，才能进行逻辑增强*/
+        InvocationHandler handler = new SubjectInvocationHandler(realSubject);
+
+        /*代理对象会使用 handler 的能力，最终实现能力增强*/
         Subject proxy = null;
-        proxy = (Subject) Proxy.newProxyInstance(delegate.getClass().getClassLoader(), delegate.getClass()
-                .getInterfaces(), handler);
+        proxy = (Subject)Proxy.newProxyInstance(realSubject.getClass()
+            .getClassLoader(), realSubject.getClass()
+            .getInterfaces(), handler);
         return proxy;
     }
 }

19e7f7274c92baf7934bfe29d022951d215cf6d5
yanlu.myl <mayanlu@newcapec.net>
2022-09-01 11:51:31 +0800

    新增Collector部分演示代码

--- src/main/java/com/java8/stream/demo2/StreamTest2.java
+++ src/main/java/com/java8/stream/demo2/StreamTest2.java
@@ -21,11 +21,11 @@ public class StreamTest2 {
      */
     private List<Employee> getAllEmployees() {
         return Stream.of(
-                new Employee("大壮","上海公司", "研发一部",  28, 3000),
-                new Employee("二牛","上海公司", "研发一部",  24, 2000),
-                new Employee("铁柱","上海公司", "研发二部",  34, 5000),
-                new Employee("翠花","南京公司", "测试一部",  27, 3000),
-                new Employee("玲玲","南京公司", "测试二部",  31, 4000)
+                new Employee("大壮", "上海公司", "研发一部", 28, 3000),
+                new Employee("二牛", "上海公司", "研发一部", 24, 2000),
+                new Employee("铁柱", "上海公司", "研发二部", 34, 5000),
+                new Employee("翠花", "南京公司", "测试一部", 27, 3000),
+                new Employee("玲玲", "南京公司", "测试二部", 31, 4000)
         ).collect(Collectors.toList());
     }
 
@@ -33,56 +33,68 @@ public class StreamTest2 {
     //现有集团内所有人员列表，需要从中筛选出上海子公司的全部人员
     @Test
     public void filterEmployeesByCompany() {
-        List<Employee> collect = getAllEmployees().stream().filter(e -> "上海公司".equals(e.getSubCompany())).collect(Collectors.toList());
+        List<Employee> collect =
+                getAllEmployees().stream().filter(e -> "上海公司".equals(e.getSubCompany())).collect(Collectors.toList());
         System.out.println(collect);
     }
 
     //现有集团内所有人员列表，需要从中筛选出上海子公司的全部人员，并按照部门进行分组
     @Test
     public void filterEmployeesThenGroup() {
-        Map<String, List<Employee>> collect = getAllEmployees().stream().filter(employee -> "上海公司".equals(employee.getSubCompany())).collect(Collectors.groupingBy(Employee::getDepartment));
+        Map<String, List<Employee>> collect =
+                getAllEmployees().stream().filter(employee -> "上海公司".equals(employee.getSubCompany())).collect(Collectors.groupingBy(Employee::getDepartment));
         System.out.println(collect);
     }
 
     //计算上海子公司每个月需要支付的员工总工资
     @Test
     public void calculateSum() {
-        Integer collect = getAllEmployees().stream().filter(employee -> "上海公司".equals(employee.getSubCompany())).collect(Collectors.summingInt(Employee::getSalary));
+        Integer collect =
+                getAllEmployees().stream().filter(employee -> "上海公司".equals(employee.getSubCompany())).collect(Collectors.summingInt(Employee::getSalary));
         System.out.println(collect);
     }
 
     //现在需要知道上海子公司里面工资最高的员工信息
     @Test
     public void findHighestSalaryEmployee() {
-        Optional<Employee> collect = getAllEmployees().stream().filter(employee -> "上海公司".equals(employee.getSubCompany())).max(Comparator.comparingInt(Employee::getSalary));
+        Optional<Employee> collect =
+                getAllEmployees().stream().filter(employee -> "上海公司".equals(employee.getSubCompany())).max(Comparator.comparingInt(Employee::getSalary));
         System.out.println(collect.get());
     }
 
     // 按照子公司分组，并统计每个子公司的员工数
     @Test
     public void groupAndCaculate() {
-        Map<String, Long> collect = getAllEmployees().stream().collect(Collectors.groupingBy(employee -> employee.getSubCompany(), Collectors.counting()));
+        Map<String, Long> collect =
+                getAllEmployees().stream().collect(Collectors.groupingBy(employee -> employee.getSubCompany(),
+                                                                         Collectors.counting()));
         System.out.println(collect);
     }
 
     //统计上海公司和非上海公司的员工总数, true表示是上海公司，false表示非上海公司
     @Test
     public void partitionByCompanyAndDepartment() {
-        Map<Boolean, Long> collect = getAllEmployees().stream().collect(Collectors.partitioningBy(e -> "上海公司".equals(e.getSubCompany()), Collectors.counting()));
+        Map<Boolean, Long> collect =
+                getAllEmployees().stream().collect(Collectors.partitioningBy(e -> "上海公司".equals(e.getSubCompany()),
+                                                                             Collectors.counting()));
         System.out.println(collect);
     }
 
     //现有整个集团全体员工的列表，需要统计各子公司内各部门下的员工人数。
     @Test
     public void groupByCompanyAndDepartment() {
-        Map<String, Map<String, Long>> collect = getAllEmployees().stream().collect(Collectors.groupingBy(e -> e.getSubCompany(), Collectors.groupingBy(e -> e.getDepartment(), Collectors.counting())));
+        Map<String, Map<String, Long>> collect =
+                getAllEmployees().stream().collect(Collectors.groupingBy(e -> e.getSubCompany(),
+                                                                         Collectors.groupingBy(e -> e.getDepartment()
+                                                                                 , Collectors.counting())));
         System.out.println(collect);
     }
 
     //给定集团所有员工列表，找出上海公司中工资最高的员工。
     @Test
     public void findHighestSalaryEmployee2() {
-        Employee collect = getAllEmployees().stream().filter(e -> "上海公司".equals(e.getSubCompany())).collect(Collectors.collectingAndThen(Collectors.maxBy(Comparator.comparingInt(Employee::getSalary)), Optional::get));
+        Employee collect =
+                getAllEmployees().stream().filter(e -> "上海公司".equals(e.getSubCompany())).collect(Collectors.collectingAndThen(Collectors.maxBy(Comparator.comparingInt(Employee::getSalary)), Optional::get));
         System.out.println(collect);
     }
 }
